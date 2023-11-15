9e7ce6e1da580ffef07b5b7a2d3e367b19a98d34
taqi <taqish12@gmail.com>
2023-11-15 10:43:16 +0500

    Update shared.module.ts

--- projects/consumer/src/app/shared/shared.module.ts
+++ projects/consumer/src/app/shared/shared.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HighlightModule,HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { CodePreviewerComponent } from './components/code-previewer/code-previewer.component';
 import { ClipboardModule } from '@angular/cdk/clipboard';
-import { DUIAccordion, DUIAlert, DUIAvatar, DUIButton, DUICheckbox, DUIChip, DUIIcon, DUIDialog, DUIInput, DUIRadio, DUISelect, DUITextarea, DUITypography, } from 'projects/david-ui-angular/src/public-api';
+import {
+  DUIAccordion,
+  DUIAlert,
+  DUIAvatar,
+  DUIButton,
+  DUICheckbox,
+  DUIChip,
+  DUIIcon,
+  DUIDialog,
+  DUIInput,
+  DUIRadio,
+  DUISelect,
+  DUITextarea,
+  DUITypography,
+} from 'projects/david-ui-angular/src/public-api';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../docs/footer/footer.component';
 import { FeaturePreviewComponent } from './components/feature-preview/feature-preview.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
-
 
 @NgModule({
   declarations: [
     CodePreviewerComponent,
     FooterComponent,
     FeaturePreviewComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
@@ -41,7 +53,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     DUIChip,
     DUIIcon
   ],
-  exports:[
+  exports: [
     CodePreviewerComponent,
     CommonModule,
     HighlightModule,
@@ -63,10 +75,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     DUITextarea,
     DUIInput,
     DUISelect,
-    DUIChip,
     DUIIcon,
     FeaturePreviewComponent,
+    DUIChip,
   ],
-
 })
-export class SharedModule { }
+export class SharedModule {}
