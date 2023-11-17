578356a5bd8b528bb2d0e9df0a8a6f3860ac35ce
taqi <taqish12@gmail.com>
2023-11-17 14:51:49 +0500

    added temp Documentation

--- projects/consumer/src/app/docs/features/collapse/collapse.component.ts
+++ projects/consumer/src/app/docs/features/collapse/collapse.component.ts
@@ -3,11 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collapse',
   templateUrl: './collapse.component.html',
-  styleUrls: ['./collapse.component.scss']
+  styleUrls: ['./collapse.component.scss'],
 })
 export class CollapseComponent {
-  open : boolean = false;
-  toggleCollapse(){
+  importCode: string = ` import { DUICollapse } from "david-ui-angular";
+
+  @NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, DUICollapse],
+    providers: [],
+    bootstrap: [AppComponent],
+  })
+  export class AppModule {}`;
+  collapseCode: string = `  <!-- Use DUI Collapse in html file -->
+<dui-collapse [open]="open">
+<div class="shadow-md w-auto mt-4">Hi there collapse component</div>
+</dui-collapse>`;
+
+  open: boolean = false;
+  toggleCollapse() {
     this.open = !this.open;
   }
 }
