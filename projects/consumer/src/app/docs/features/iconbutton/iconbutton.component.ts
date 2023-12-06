51f48d8fe3f38121f27990b8201f7edbf91f13f4
taqi <taqish12@gmail.com>
2023-12-06 14:23:51 +0500

    Icon button docs

--- projects/consumer/src/app/docs/features/iconbutton/iconbutton.component.ts
+++ projects/consumer/src/app/docs/features/iconbutton/iconbutton.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-
 @Component({
   selector: 'app-iconbutton',
   templateUrl: './iconbutton.component.html',
-  styleUrls: ['./iconbutton.component.scss']
+  styleUrls: ['./iconbutton.component.scss'],
 })
 export class IconbuttonComponent {
-
-  coffeIcon = faHeart;
+  heartIcon = faHeart;
 
   public importCode = ` import { DUIIconButton } from "david-ui-angular";
 
@@ -20,10 +18,91 @@ export class IconbuttonComponent {
     bootstrap: [AppComponent],
   })
   export class AppModule {}`;
-  buttonCode = ` <!-- Use DUI Button in html file -->
+  buttonCode = ` 
+  import { faHeart } from '@fortawesome/free-solid-svg-icons';
+  heartIcon = faHeart;
+
+  <!-- Use DUI Button in html file -->
   <dui-icon-button color="gray">
-    <fa-icon [icon]="coffeIcon"></fa-icon>
+    <fa-icon [icon]="heartIcon"></fa-icon>
   </dui-icon-button>  `;
+  outlinedButton = `
+  import { faHeart } from '@fortawesome/free-solid-svg-icons';
+  heartIcon = faHeart;
+  <!-- Use DUI Button in html file -->
+    <dui-icon-button size="md" variant="filled" color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button size="md" variant="outlined" color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button size="md" variant="text" color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button size="md" variant="gradient" color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  `;
 
+  buttonSizeCode = ` 
+  import { faHeart } from '@fortawesome/free-solid-svg-icons';
+  heartIcon = faHeart;
 
+  <!-- Use DUI Button in html file -->
+  <dui-icon-button size="sm" color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button size="md"  color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button size="lg"  color="gray">
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  `;
+  buttonColorCode = `
+  <div class="flex items-center gap-4">
+    <dui-icon-button  color="gray">
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button   color="blue">
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button   color="red">
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button   color="yellow">
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button   color="green">
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+
+  </div>
+  `;
+  roundedButtons: string = `
+  <div class="flex items-center gap-4">
+    <dui-icon-button [rounded]="true" color="gray" variant="filled" >
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button [rounded]="true" color="gray" variant="outlined" >
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button [rounded]="true" color="gray" variant="text" >
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+    <dui-icon-button [rounded]="true" color="gray" variant="gradient" >
+      <fa-icon [icon]="heartIcon"></fa-icon>
+    </dui-icon-button>
+
+  </div>
+  `;
+
+  rippleEffectButton: string = ` 
+  <dui-icon-button [ripple]="true" color="gray" variant="filled" >
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  <dui-icon-button [ripple]="false" color="gray" variant="filled" >
+    <fa-icon [icon]="heartIcon"></fa-icon>
+  </dui-icon-button>
+  `;
 }
