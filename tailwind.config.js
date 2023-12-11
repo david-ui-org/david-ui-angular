60ff0557210b7dd886faec096c9f66cfd9dae7ab
taqi <taqish12@gmail.com>
2023-12-12 01:54:26 +0500

    temp error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/html/utils/withMT");
+// const withMT = require("@material-tailwind/html/utils/withMT");
 
 module.exports = {
   future: {
